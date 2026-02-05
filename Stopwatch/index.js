57262f75aae1110c3fd69a20a9054aeb3294c4a4
caicedo1236 <lmapps5154@student.pps.net>
2026-02-05 14:42:04 -0800

    Implement stopwatch functionality with start, stop, reset
    Added functionality to start, stop, and reset a stopwatch.

--- Stopwatch/index.js
+++ Stopwatch/index.js
@@ -1,2 +1,29 @@
-let seconds = 0;
-document.getElementById("start").addEventListener("click")
+const start = document.getElementById("start");
+const time = document.getElementById("time");
+const stop = document.getElementById("stop");
+const reset = document.getElementById("reset");
+
+let seconds = 0.0;
+let timerId= null;
+
+function count(){
+    time.textContent = seconds;
+    seconds++
+}
+stop.addEventListener("click", () => {
+    clearInterval(timerId);
+    timerId = null
+})
+reset.addEventListener("click", () => {
+    clearInterval(timerId);
+    timerId = null;
+    seconds = 0;
+    time.textContent = 0;
+
+    }
+);
+start.addEventListener("click", () => {
+    if (timerId === null) {      
+        timerId = setInterval(count, 1000);
+    }
+});
